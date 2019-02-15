798fbc3dd4366ae1a81a3a38cddda1fea6d06ec9
aarjua <aarju.a786599@gmail.com>
2019-02-15 22:13:07 +0530

    add

--- patanjali/application/views/apply.php
+++ patanjali/application/views/apply.php
@@ -7,10 +7,10 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script>
 function openWin() {
-  window.open("http://localhost/CodeIgniter/index.php/welcome/apply1");
+  window.open("<?php echo base_url();?>index.php/welcome/apply1");
 }
 function openWin1() {
-  window.open("http://localhost/CodeIgniter/index.php/welcome/login1");
+  window.open("<?php echo base_url();?>index.php/welcome/login1");
 }
 </script>
 
